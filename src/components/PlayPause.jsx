96cad3446f44a4b40683cf26d3d4480065641bef
MinhTranPDX <104175908+MinhTranPDX@users.noreply.github.com>
2022-10-25 23:35:11 -0700

    finish song card component

--- src/components/PlayPause.jsx
+++ src/components/PlayPause.jsx
@@ -1,5 +1,19 @@
-const PlayPause = () => (
-  <div>Loader</div>
-);
+import { FaPauseCircle, FaPlayCircle } from 'react-icons/fa';
+
+// if song is playing and activeSong.title === song.title, show pause circle, else show play circle
+
+const PlayPause = ({ isPlaying, activeSong, song, handlePause, handlePlay }) => (isPlaying && activeSong?.title === song.title ? (
+  <FaPauseCircle
+    size={35}
+    className="text-gray-300"
+    onClick={handlePause}
+  />
+) : (
+  <FaPlayCircle
+    size={35}
+    className="text-gray-300"
+    onClick={handlePlay}
+  />
+));
 
 export default PlayPause;
