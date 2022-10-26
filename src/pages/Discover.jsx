d3c6ae599f0e139fc5da4fabcf22abaeb66d4b52
MinhTranPDX <104175908+MinhTranPDX@users.noreply.github.com>
2022-10-26 00:03:56 -0700

    Implement Play/Pause functionality

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -17,10 +17,8 @@ const Discover = () => {
 
   return (
     <div className="flex flex-col">
-      <div className="w-full flex justify-between items-center
-        sm:flex-row flex-col mt-4 mb-10"
-      >
-        <h2 className="font-bold text-3xl text-white">Discover {genreTitle}</h2>
+      <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
+        <h2 className="font-bold text-3xl text-white text-left">Discover {genreTitle}</h2>
         <select
           onChange={() => {}}
           value=""
@@ -28,18 +26,19 @@ const Discover = () => {
         >
           {genres.map((genre) => <option key={genre.value} value={genre.value}>{genre.title}</option>)}
         </select>
-        <div className="flex flex-wrap sm:justify-start justify-center gap-8">
-          {data?.map((song, i) => (
-            <SongCard
-              key={song.key}
-              song={song}
-              isPlaying={isPlaying}
-              activeSong={activeSong}
-              data={data}
-              i={i}
-            />
-          ))}
-        </div>
+      </div>
+
+      <div className="flex flex-wrap sm:justify-start justify-center gap-8">
+        {data?.map((song, i) => (
+          <SongCard
+            key={song.key}
+            song={song}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            data={data}
+            i={i}
+          />
+        ))}
       </div>
 
     </div>
