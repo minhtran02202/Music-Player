cdb6574f2a1adf8e9edb240a2944ef4aa99062aa
MinhTranPDX <104175908+MinhTranPDX@users.noreply.github.com>
2022-10-30 17:33:44 -0700

    Finish

--- src/components/RelatedSongs.jsx
+++ src/components/RelatedSongs.jsx
@@ -1,6 +1,6 @@
 import SongBar from './SongBar';
 
-const RelatedSongs = ({data, isPlaying, activeSong,
+const RelatedSongs = ({ data, isPlaying, activeSong,
   handlePauseClick, handlePlayClick, artistId }) => (
     <div className="flex flex-col">
       <h1 className="font-bold text-3xl text-white">Related Songs:</h1>
